7941a4e8e29962018ea2f141588e2af801fe567f
Isaac Joseph <ijoseph@smu.edu>
2018-09-11 18:46:45 -0500

    Updated Secondary Parsing

--- Sprint1/MadLibs/main.cpp
+++ Sprint1/MadLibs/main.cpp
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <algorithm>
 #include <sstream>
+#include <time.h>
+#include <stdlib.h>
 
 
 using namespace std;
@@ -20,13 +22,18 @@ void createTweet(string a, string b);
 void printAll();
 ifstream myfile;
 ifstream myfile2;
+ofstream myfile3;
 vector<User> users;
 vector<string> information;
+vector<string> information2;
 
 void parseData();
-
+void parseData2();
+void printTypes();
+void printUsers();
 int main(int argc, char *argv[])
 {
+    srand(time(NULL));
     myfile.open(argv[1]);
     string c;
     getline(myfile, c);
@@ -42,28 +49,74 @@ int main(int argc, char *argv[])
     myfile.close();
 
     parseData();
-    printAll();
-
+    //printUsers();
 
 
     myfile2.open(argv[2]);
     if (myfile2.is_open()){
         while(!myfile2.eof()){
+            getline(myfile2, c);
+            information2.push_back(c);
         }
     }
     else{
         cout << "Unable to open Mad libs file";
     }
+    myfile2.close();
+    parseData2();
+    myfile3.open(argv[3]);
+    if (myfile3.is_open()){
+        for(unsigned int i = 0; i<information2.size(); i++){
+
+            myfile3<<information2[i]<<endl;
+        }
+    }
+    myfile3.close();
+
     return 0;
 }
 
+//fix this
+void printTypes(){
+    vector<string> POS(1);
+    int count = 0;
+    for(unsigned int i =0; i<users.size(); i++){
+        cout<<"yikes"<<endl;
+        for(unsigned int j =0; j<users[i].getSizeofVector(); j++){
+            for(unsigned int s = 0; s<=POS.size(); s++){
+                count = 0;
+                if(users[i].accessTweet(j).getPOS() != POS[s]){
+                    count++;
+               }
+                if(count == (POS.size())){
+                    POS.push_back(users[i].accessTweet(j).getPOS());
+                    goto label;
+                }
+            }
+        label:
+        int cool;
+        }
+   }
+    for(unsigned int p =0; p<POS.size(); p++){
+        cout<<POS[p]<<endl;
+    }
+}
+
+void printUsers(){
+    for(unsigned int i = 0; i<users.size(); i++){
+        cout<<users[i].getUsername()<<endl;
+    }
+}
+
 void printAll(){
-    for(int i =0; i<users.size(); i++){
-        for(int j =0; j<users[i].getSizeofVector(); j++){
+    for(unsigned int i =0; i<users.size(); i++){
+        for(unsigned int j =0; j<users[i].getSizeofVector(); j++){
             cout<<users[i].getUsername()<<"-"<<users[i].accessTweet(j).getId()<<endl;
             cout<<users[i].accessTweet(j).getWord()<<"-"<<users[i].accessTweet(j).getPOS()<<endl;
         }
     }
+
+
 }
 
 void createUser(string a){
@@ -95,7 +148,7 @@ void parseData(){
         string hold3;
         getline(streamHold, hold3);
         int counter = 0;
-        for(int i =0; i<hold2.length();i++){
+        for(unsigned int i =0; i<hold2.length();i++){
 
             if((hold2[i] == '(') || (hold2[i] == ')')){
                 counter++;
@@ -115,7 +168,7 @@ void parseData(){
             string input1;
             string input2;
             getline(streamHold2, junk, '\'');
-            for(int i =0; i<junk.length();i++){
+            for(unsigned int i =0; i<junk.length();i++){
                 if(junk[i] == '"'){
                     if(junk[i+1] == '"'){
                         string hold;
@@ -135,7 +188,7 @@ void parseData(){
             label:
             getline(streamHold2, junk, '\'');
             getline(streamHold2, input2, '\'');
-            for(int i =0; i<users.size();i++){
+            for(unsigned int i =0; i<users.size();i++){
                 if(users[i].getUsername() == username){
                     if(hold3[1] == '0'){
                         users[i].addTweet(Tweet (id, input1, input2, 1, 0));
@@ -148,5 +201,100 @@ void parseData(){
 
             loops--;
         }
+    }
+}
+
+void parseData2(){
+    string username;
+    string hold;
+    string sent1;
+    string sent2;
+    string subIn;
+    int counter = 0;
+    vector<string> subInVec(0);
+    for(unsigned int y=0; y<information2.size()-1;y++){
+        stringstream infoStream (information2[y]);
+        getline(infoStream, username, ' ');
+        getline(infoStream, hold);
+       // for(int i =0; i<hold.length();i++){
+       //     if(hold[i] == '['){
+       //         counter++;
+       //     }
+       //}
+       // vector<string> parsedString (counter+1);
+        //vector<string> subIn(counter);
+        //int x =0;
+        //while(counter!= 0){
+        //    stringstream infoStream2 (hold);
+        //    getline(infoStream2, parsedString[x], '[');
+        //    cout<< parsedString[x]<<":sent"<<endl;
+        //    getline(infoStream2, subIn[x], ']');
+        //    cout<< subIn[x]<<":fill"<<endl;
+        //    if(counter ==1){
+        //        x++;
+        //        getline(infoStream2, parsedString[x]);
+        //        cout<<parsedString[x]<<":sent"<<endl;
+        //    }
+        //    x++;
+        //    counter--;
+        //}
+        //vector <vector <string>> subIn2(subIn.size());
+        //for(int i=0; i<subIn.size();i++){
+        //    for(int j =0; j<users.size(); j++){
+        //        if(users[j].getUsername() == username){
+                    //cout<<users[j].getUsername()<<endl;
+         //           for(int k = 0; k<users[j].getSizeofVector(); k++){
+          //              if(subIn[i] == users[j].accessTweet(k).getPOS()){
+           //                 subIn2[i].push_back((users[j].accessTweet(k).getWord()));
+                            //cout<<(users[j].accessTweet(k).getWord())<<endl;
+             //           }
+               //     }
+               // }
+           // }
+       // }
+        stringstream infoStream2 (hold);
+        getline(infoStream2, sent1, '[');
+        getline(infoStream2, subIn, ']');
+        getline(infoStream2, sent2);
+
+        for(unsigned int i =0; i<users.size();i++){
+            if(users[i].getUsername() == username){
+                cout<<users[i].getUsername()<<endl;
+                for(unsigned int j =0; j<users[i].getSizeofVector(); j++){
+                    if(subIn == users[i].accessTweet(j).getPOS()){
+                        //cout<< users[i].accessTweet(j).getPOS()<<endl;
+                        string c = users[i].accessTweet(j).getWord();
+                        subInVec.push_back(c);
+                        for(int l= 0; l<subInVec.size();l++){
+                            cout<<subInVec[l]<<endl;
+                        }
+
+
+                    }
+                }
+            }
+        }
+        information2[y] = username + " ";
+        information2[y] = information2[y] + sent1;
+        int v = 0;//rand()%(subInVec.size());
+        cout<<v<<endl;
+        cout<<subInVec.size()<<endl;
+        subIn = subInVec[v];
+        information2[y] = information2[y] + "["+subIn+"]";
+        information2[y] = information2[y] + sent2;
+        subInVec.clear();
+        //for(int i = 0; i<parsedString.size(); i++){
+        //    if(i == subIn.size()){
+        //        information2[y] = information2[y]  + parsedString[i];
+        //    }
+        //    else{
+        //        information2[y] = information2[y]  + parsedString[i];
+        //        int v = rand()%(subIn2[i].size());
+        //        subIn[i] = subIn2[i][v];
+        //        information2[y] = information2[y]  + "[" +subIn[i] + "]";
+        //    }
+
+       // }
+
     }
 }
