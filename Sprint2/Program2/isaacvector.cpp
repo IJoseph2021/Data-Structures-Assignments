635c16c02d4bed6bae7f30e4fc813e60b46d0ad7
Isaac Joseph <ijoseph@smu.edu>
2018-09-20 18:41:17 -0500

    Testing

---
+++ Sprint2/Program2/isaacvector.cpp
@@ -0,0 +1,6 @@
+#include "isaacvector.h"
+
+IsaacVector::IsaacVector()
+{
+
+}
---
+++ Sprint2/Program2/isaacvector.h
@@ -0,0 +1,11 @@
+#ifndef ISAACVECTOR_H
+#define ISAACVECTOR_H
+
+
+class IsaacVector
+{
+public:
+    IsaacVector();
+};
+
+#endif // ISAACVECTOR_H
