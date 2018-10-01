4331038d1b1a427a600e3ddefbe7bb7af670dca1
Isaac Joseph <ijoseph@smu.edu>
2018-10-01 18:24:54 -0500

    removes test

--- Sprint3/Program3/main.cpp
+++ Sprint3/Program3/main.cpp
@@ -1,9 +1,91 @@
 #include <iostream>
+#include <time.h>
+#include <vector>
+#include <stdio.h>
+#include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
+vector <string> hold;
+vector <vector<string>> wordLength (20);
+void sortVector();
+int partitionVector(vector<string>&a, int b, int c);
+void quickSortVector(vector<string>& a, int b, int c);
 
-int main()
+fstream myfile;
+ofstream myfile2;
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    string a;
+    srand (time(NULL));
+    myfile.open(argv[1]);
+    if(myfile.is_open()){
+        while(!myfile.eof()){
+            getline(myfile, a);
+            hold.push_back(a);
+        }
+    }
+   myfile.close();
+   sortVector();
+   for(int i =0; i<wordLength.size(); i++){
+       quickSortVector(wordLength[i], 0, wordLength[i].size()-1);
+   }
+    myfile2.open(argv[2]);
+    if(myfile2.is_open()){
+        for(int i =0; i<wordLength.size(); i++){
+            myfile2<<i<<":"<<endl;
+            for(int j =0; j<wordLength[i].size(); j++){
+                myfile2<<wordLength[i][j]<<endl;
+            }
+        }
+    }
+    myfile2.close();
     return 0;
+}
+
+void sortVector(){
+    int x = 0;
+    for(int i =0; i<hold.size(); i++){
+        x = hold[i].length()-1;
+        wordLength[x].push_back(hold[i]);
+    }
+}
+
+int partitionVector(vector<string> &a, int b, int c){
+
+    if(a.size()>1){
+        int random = b + rand() % (c - b);
+        string pivot = a[random];
+        int i = b - 1;
+        int j = c + 1;
+        while (true) {
+
+            do {
+                j--;
+            } while (a[j] > pivot);
+
+            do {
+                i++;
+            } while (a[i] < pivot);
+
+            if (i < j){
+                swap(a[i], a[j]);
+            }
+            else{
+                return j;
+            }
+        }
+    }
+}
+
+
+void quickSortVector(vector<string> &a, int b, int c){
+    int r;
+    if(b<c){
+        r = partitionVector(a,b, c);
+        quickSortVector(a, b, r);
+        quickSortVector(a, r+1, c);
+    }
 }
