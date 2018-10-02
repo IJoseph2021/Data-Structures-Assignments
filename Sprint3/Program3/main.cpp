66a5dfb1d7c5679053e76c7a47487414aca0748b
Isaac Joseph <ijoseph@smu.edu>
2018-10-02 12:38:56 -0500

    Array Replacement

--- Sprint3/Program3/main.cpp
+++ Sprint3/Program3/main.cpp
@@ -1,41 +1,52 @@
 #include <iostream>
+#include <time.h>
 #include <vector>
-#include <string>
-#include <fstream>
 #include <stdio.h>
 #include <cstdlib>
+#include <fstream>
+#include <vector>
+#include <string>
 #include <sstream>
 using namespace std;
+//vector <string> hold;
+vector <vector<string>> wordLength (20);
+void sortVector(string hold[]);
+int partitionVector(vector<string>&a, int b, int c);
+void quickSortVector(vector<string>& a, int b, int c);
+
 fstream myfile;
 ofstream myfile2;
-
-vector <string> hold;
-vector <vector<string>> wordLength (20);
-void sortVector();
-void bubbleSort(vector<string>&a, int b);
-int main(int argc, char* argv[1])
+int main(int argc, char *argv[])
 {
+
     string a;
+    srand (time(NULL));
     myfile.open(argv[1]);
     getline(myfile, a);
-    getline(myfile, a);
     stringstream input(a);
-    int x = 0;
-    input >> x;
-    if (myfile.is_open()){
+    int y =0;
+    input>>y;
+    string * hold = new string[y];
+    getline(myfile, a);
+    stringstream input1(a);
+    int x =0;
+    input1>>x;
+    int q =0;
+    if(myfile.is_open()){
         while(!myfile.eof()){
             getline(myfile, a);
-            hold.push_back(a);
+            //hold.push_back(a);
+            hold[q] = a;
+            q++;
         }
     }
-    myfile.close();
-    sortVector();
-    for(int i =0; i<wordLength.size(); i++){
-        bubbleSort(wordLength[i], wordLength[i].size());
-    }
+   myfile.close();
+   sortVector(hold);
+   for(int i =0; i<wordLength.size(); i++){
+       quickSortVector(wordLength[i], 0, wordLength[i].size()-1);
+   }
     myfile2.open(argv[2]);
     if(myfile2.is_open()){
-
         for(int i =0; i<wordLength.size(); i++){
             for(int j =0; j<wordLength[i].size(); j++){
                 myfile2<<wordLength[i][j]<<endl;
@@ -44,27 +55,67 @@ int main(int argc, char* argv[1])
                     goto label;
                 }
             }
-
         }
     }
     label:
     myfile2.close();
+    return 0;
 }
 
-void sortVector(){
+void sortVector(string hold[]){
     int x = 0;
-    for(int i =0; i<hold.size(); i++){
+    for(int i =0; i<sizeof(hold)/sizeof(hold[0]); i++){
         x = hold[i].length()-1;
         wordLength[x].push_back(hold[i]);
     }
 }
 
-void bubbleSort(vector<string> &a, int b){
-    for(int i =0; i<b-1; i++){
-        for(int j = 0; j<b - i - 1; j++){
-            if(a[j] > a[j+1]){
-                swap(a[j], a[j+1]);
+int partitionVector(vector<string> &a, int b, int c){
+
+    if(a.size()>1){
+        int random = b + rand() % (c - b);
+        string u = a[random];
+        random = b + rand() % (c - b);
+        string o = a[random];
+        random = b + rand() % (c - b);
+        string p = a[random];
+        string pivot;
+        if((u<o && u>p) || (u>o && u<p)){
+            pivot = u;
+        }
+        else if((o<u && o>p) || (o>u && o<p)){
+           pivot = o;
+        }
+        else{
+            pivot = p;
+        }
+        //string pivot = a[random];
+        int i = b - 1;
+        int j = c + 1;
+        while (true) {
+            do {
+                j--;
+            } while (a[j] > pivot);
+            do {
+                i++;
+            } while (a[i] < pivot);
+
+            if (i < j){
+                swap(a[i], a[j]);
+            }
+            else{
+                return j;
             }
         }
+    }
+}
+
+
+void quickSortVector(vector<string> &a, int b, int c){
+    int r;
+    if(b<c){
+        r = partitionVector(a,b, c);
+        quickSortVector(a, b, r);
+        quickSortVector(a, r+1, c);
     }
 }
