7b0e9d9d0fece401c264e12f91e70263984f1f3a
Isaac Joseph <ijoseph@smu.edu>
2018-10-06 01:03:38 -0500

    Test Vect + array

--- Sprint3/Program3/main.cpp
+++ Sprint3/Program3/main.cpp
@@ -10,9 +10,10 @@
 using namespace std;
 vector <string> hold;
 vector <vector<string>> wordLength (20);
-void sortVector();
+void sortVector(string **a, int b[], vector<string> c);
 int partitionVector(vector<string>&a, int b, int c);
 void quickSortVector(vector<string>& a, int b, int c);
+int y =0;
 
 fstream myfile;
 ofstream myfile2;
@@ -23,10 +24,18 @@ int main(int argc, char *argv[])
     srand (time(NULL));
     myfile.open(argv[1]);
     getline(myfile, a);
+    stringstream input1(a);
+    int y =0;
+    input1>>y;
     getline(myfile, a);
     stringstream input(a);
     int x =0;
     input>>x;
+    string ** arr = new string* [31];
+    int* arrCount  = new int [31];
+        for(int i =0; i<31;i++){
+            arr[i] = new string[y];
+    }
     if(myfile.is_open()){
         while(!myfile.eof()){
             getline(myfile, a);
@@ -34,7 +43,7 @@ int main(int argc, char *argv[])
         }
     }
    myfile.close();
-   sortVector();
+   sortVector(arr, arrCount, hold);
    for(int i =0; i<wordLength.size(); i++){
        quickSortVector(wordLength[i], 0, wordLength[i].size()-1);
    }
@@ -55,13 +64,18 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-void sortVector(){
+void sortVector(string **a, int b[], vector<string> c){
     int x = 0;
-    for(int i =0; i<hold.size(); i++){
-        x = hold[i].length()-1;
-        wordLength[x].push_back(hold[i]);
+    for(int i =0; i<31; i++){
+        b[i] =0;
     }
+    for(int i =0; i<y; i++){
+        x = c[i].length()-1;
+        a[x][b[x]] = c[i];
+        b[x] = b[x] + 1;
+        }
 }
+
 
 int partitionVector(vector<string> &a, int b, int c){
 
