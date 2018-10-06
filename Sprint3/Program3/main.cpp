7a3237e9898b4452a3f7ad705f5107b4a4bf2e4b
Isaac Joseph <ijoseph@smu.edu>
2018-10-06 00:20:43 -0500

    Insertion sort test

--- Sprint3/Program3/main.cpp
+++ Sprint3/Program3/main.cpp
@@ -8,50 +8,71 @@
 #include <string>
 #include <sstream>
 using namespace std;
-vector <string> hold;
-vector <vector<string>> wordLength (20);
-void sortVector();
-const int RUN = 32;
-int partitionVector(vector<string>&a, int b, int c);
-void quickSortVector(vector<string>& a, int b, int c);
-void insertionSort(vector <string> &arr, int left, int right);
-void merge(vector <string> &arr, int l, int m, int r);
-void timSort(vector <string> &arr, int n) ;
-void DualPivotQuickSort(vector <string> &arr, int low, int high);
-int partition(vector <string> &arr, int low, int high, int* lp);
-void partition(vector<string> & a, int l, int r, int &i, int &j) ;
-void quicksort(vector <string> &a, int l, int r);
+//vector <string> hold;
+//vector <vector<string>> wordLength (20);
+//int y =0;
+void sortVector(string **a, int b[], string c[]);
+int partitionVector(string a[], int b, int c, int d);
+void quickSortVector(string a [], int b, int c, int d);
+void DualPivotQuickSort(string * arr, int low, int high);
+int partition(string* arr, int low, int high, int* lp);
+void optimizedQuickSort(string *arr, int b, int c, int d);
+void insertionSort(string * arr, int b, int c);
+//void insertionSort(string arr[], int n);
+//void bubbleSort(string arr[], int n);
+int y = 0;
 
 
-fstream myfile;
-ofstream myfile2;
-int main(int argc, char *argv[])
+//fstream myfile;
+//fstream myfile2;
+
+
+int main(int argc, const char *argv[])
+
 {
+    ifstream myfile;
+    ofstream myfile2;
+    string a ="";
 
-    string a;
     srand (time(NULL));
     myfile.open(argv[1]);
     getline(myfile, a);
+    stringstream input1(a);
+    //int y =0;
+    input1>>y;
     getline(myfile, a);
     stringstream input(a);
     int x =0;
     input>>x;
+    string ** arr = new string* [30];
+    for(int i =0; i<20;i++){
+        arr[i] = new string[y];
+    }
+    int* arrCount  = new int [30];
+    string* hold = new string[y+1];
+    int q =0;
     if(myfile.is_open()){
         while(!myfile.eof()){
             getline(myfile, a);
-            hold.push_back(a);
+            hold[q] = a;
+            q++;
         }
     }
    myfile.close();
-   sortVector();
-   for(int i =0; i<wordLength.size(); i++){
-      quicksort(wordLength[i], 0, wordLength[i].size()-1);
+   sortVector(arr, arrCount, hold);
+   delete []hold;
+   for(int i =0; i<30; i++){
+
+       //optimizedQuickSort(arr[i], 0, arrCount[i]-1, 750);
+       insertionSort(arr[i], 0, arrCount[i] - 1);
+       //insertionSort(arr[i], (sizeof(arr[i])/sizeof(arr[i][0])));
+       //bubbleSort(arr[i], (sizeof(arr[i])/sizeof(arr[i][0])));
    }
     myfile2.open(argv[2]);
     if(myfile2.is_open()){
-        for(int i =0; i<wordLength.size(); i++){
-            for(int j =0; j<wordLength[i].size(); j++){
-                myfile2<<wordLength[i][j]<<endl;
+        for(int i =0; i<30; i++){
+            for(int j =0; j<arrCount[i]; j++){
+                myfile2<<arr[i][j]<<endl;
                 x--;
                 if(x<1){
                     goto label;
@@ -61,87 +82,104 @@ int main(int argc, char *argv[])
     }
     label:
     myfile2.close();
+    delete arr;
     return 0;
 }
 
 
-void sortVector(){
+
+void sortVector(string **a, int b[], string c[]){
     int x = 0;
-    for(int i =0; i<hold.size(); i++){
-        x = hold[i].length()-1;
-        wordLength[x].push_back(hold[i]);
+    int counter =0;
+    for(int i =0; i<30; i++){
+        b[i] =0;
+    }
+    for(int i =0; i<y; i++){
+        x = c[i].length()-1;
+        a[x][b[x]] = c[i];
+        b[x] = b[x] + 1;
     }
 }
 
-void partition(vector<string> & a, int l, int r, int &i, int &j)
+
+
+void DualPivotQuickSort(string * arr, int low, int high)
 {
-    i = l-1, j = r;
-    int p = l-1, q = r;
-    string v = a[r];
-
-    while (true)
-    {
-        // From left, find the first element greater than
-        // or equal to v. This loop will definitely terminate
-        // as v is last element
-        while (a[++i] < v);
-
-        // From right, find the first element smaller than or
-        // equal to v
-        while (v < a[--j])
-            if (j == l)
-                break;
-
-        // If i and j cross, then we are done
-        if (i >= j) break;
-
-        // Swap, so that smaller goes on left greater goes on right
-        swap(a[i], a[j]);
-
-        // Move all same left occurrence of pivot to beginning of
-        // array and keep count using p
-        if (a[i] == v)
-        {
-            p++;
-            swap(a[p], a[i]);
-        }
-
-        // Move all same right occurrence of pivot to end of array
-        // and keep count using q
-        if (a[j] == v)
-        {
-            q--;
-            swap(a[j], a[q]);
-        }
+    if (low < high) {
+        // lp means left pivot, and rp means right pivot.
+        int lp, rp;
+        rp = partition(arr, low, high, &lp);
+        DualPivotQuickSort(arr, low, lp - 1);
+        DualPivotQuickSort(arr, lp + 1, rp - 1);
+        DualPivotQuickSort(arr, rp + 1, high);
     }
-
-    // Move pivot element to its correct index
-    swap(a[i], a[r]);
-
-    // Move all left same occurrences from beginning
-    // to adjacent to arr[i]
-    j = i-1;
-    for (int k = l; k < p; k++, j--)
-        swap(a[k], a[j]);
-
-    // Move all right same occurrences from end
-    // to adjacent to arr[i]
-    i = i+1;
-    for (int k = r-1; k > q; k--, i++)
-        swap(a[i], a[k]);
 }
 
-// 3-way partition based quick sort
-void quicksort(vector <string> &a, int l, int r)
+int partition(string* arr, int low, int high, int* lp)
 {
-    if (r <= l) return;
+    if (arr[low] > arr[high])
+        swap(arr[low], arr[high]);
+    // p is the left pivot, and q is the right pivot.
+    int j = low + 1;
+    int g = high - 1, k = low + 1;
+    string p = arr[low], q = arr[high];
+    while (k <= g) {
 
-    int i, j;
+        // if elements are less than the left pivot
+        if (arr[k] < p) {
+            swap(arr[k], arr[j]);
+            j++;
+        }
 
-    // Note that i and j are passed as reference
-    partition(a, l, r, i, j);
+        // if elements are greater than or equal
+        // to the right pivot
+        else if (arr[k] >= q) {
+            while (arr[g] > q && k < g)
+                g--;
+            swap(arr[k], arr[g]);
+            g--;
+            if (arr[k] < p) {
+                swap(arr[k], arr[j]);
+                j++;
+            }
+        }
+        k++;
+    }
+    j--;
+    g++;
 
-    // Recur
-    quicksort(a, l, j);
-    quicksort(a, i, r);
+    // bring pivots to their appropriate positions.
+    swap(arr[low], arr[j]);
+    swap(arr[high], arr[g]);
+
+    // returning the indeces of the pivots.
+    *lp = j; // because we cannot return two elements
+             // from a function.
+
+    return g;
+}
+
+void insertionSort(string * arr, int b, int c){
+    for(int i = b + 1; i<=c; i++){
+        string value = arr[i];
+        int j =i;
+        while(j>b && arr[j-1] > value){
+            arr[j] = arr[j-1];
+            j--;
+        }
+        arr[j] = value;
+    }
+}
+
+void optimizedQuickSort(string *arr, int b, int c, int d){
+    if (b<c) {
+        if ((c - b + 1) > d) {
+            int lp, rp;
+            rp = partition(arr, b, c, &lp);
+            DualPivotQuickSort(arr, b, lp - 1);
+            DualPivotQuickSort(arr, lp + 1, rp - 1);
+            DualPivotQuickSort(arr, rp + 1, c);
+
+        }
+    }
 }
